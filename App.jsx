2bb61a482c2723c6361e2d2bb37f18336f414ea1
sejacriativoaqui-jpg <sejacriativoaqui@gmail.com>
2026-09-04 11:23:25 -0300

    Delete App.jsx

--- App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-  return (
-    <div style={{ padding: "40px", fontFamily: "Arial" }}>
-      <h1>Orkut 2026</h1>
-      <p>Site carregando corretamente.</p>
-    </div>
-  );
-}
