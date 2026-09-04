2d0483cbdc7d7a3ab4882c5210cb00437c1dc723
sejacriativoaqui-jpg <sejacriativoaqui@gmail.com>
2026-09-04 11:15:44 -0300

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>Orkut 2026</h1>
+      <p>Site carregando corretamente.</p>
+    </div>
+  );
+}
